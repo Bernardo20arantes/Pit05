ad3b5d9b3c48c2460e529ad25fcfe9d15c07236c
Bernardo20arantes <118383652+Bernardo20arantes@users.noreply.github.com>
2023-10-25 00:35:55 +0000

    pdo01

---
+++ PDO/pdo.php
@@ -0,0 +1,19 @@
+<?php 
+
+$servidor ="localhost";
+$usuario = "root";
+$senha = "";
+$dbname = "formulario-santosh";
+
+$conexao = mysqli_connect($servidor,$usuario,$senha,$dbname);
+
+/*if(!$conexao){
+
+    die("Houve um erro : " .mysqli_connect_error());
+}
+else{
+    echo "Deu certo";
+}
+*/
+
+?>
